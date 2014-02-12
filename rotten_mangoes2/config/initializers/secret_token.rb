883c10a7f84c9bdc7af7082a8c9b42b684cb7fe8
Tom Sav <tomsav2010@gmail.com>
2014-02-12 18:40:47 +0000

    First Commit

---
+++ rotten_mangoes2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes2::Application.config.secret_key_base = 'c4ceb53cbba72b26c667b6b6d40d94ae86e9557a78f980283e36d9d7e1cc98e4eab9f72be55a508cb58bdb4481d1451fc76b885c4705d6c2ebce14c93e4e412b'
---
+++ rotten_mangoes2/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RottenMangoes2::Application.config.session_store :cookie_store, key: '_rotten_mangoes2_session'
